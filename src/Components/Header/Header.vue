5834d404c9fafa3cf65d886c887ecaaef4711d1c
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-09-11 10:46:47 +0100

    add header

---
+++ src/Components/Header/Header.vue
@@ -0,0 +1,117 @@
+<template>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 space-y-5 header">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+        >
+          <div class="flex flex-shrink-0 items-center title-desktop">
+            <span><strong> Sabbbat </strong> Lumpatshia</span>
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <a
+                class="title-link"
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <a href="https://github.com/Sabbat10" target="_blank">
+            <button
+              type="button"
+              class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">View notifications</span>
+              <img
+                src="../../../public/icones/github.svg"
+                width="45"
+                height="40"
+                alt="icone-github"
+              />
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2 title-mobile">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+import symbole from "../../../public/icones/symbole.png";
+
+const navigation = [
+  //   { name: "Sabbat Lumpatshia", href: "#", current: true },
+  { name: "Home", href: "#", current: false },
+  { name: "About", href: "#", current: false },
+  { name: "Work", href: "#", current: false },
+];
+</script>
+
+<style scoped>
+.header {
+  padding: 12px;
+}
+.title-desktop {
+  font-size: 25.5px;
+  text-align: left;
+}
+.title-mobile {
+  font-size: 10.5px;
+  color: #ffffff;
+  text-align: left;
+}
+strong {
+  font-weight: 900;
+  color: #ffffff;
+}
+.title-link {
+  font-size: 18.5px;
+}
+</style>
--- src/Components/Layoute.vue
+++ src/Components/Layoute.vue
@@ -1,18 +1,19 @@
-<template>
-  <div class="bg-blue-500 text-white p-4 rounded-md shadow-md">
-    <h1 class="text-2xl font-bold">{{ name }}</h1>
-  </div>
-  <button class="btn btn-primary">One</button>
-  <button class="btn btn-secondary">Two</button>
-  <button class="btn btn-accent btn-outline">Three</button>
-</template>
-
+<!-- script -->
 <script setup>
-const name = "Portfolio-Sabbat";
+import { ref } from "vue";
+import Header from "./Header/Header.vue";
 </script>
 
+<!-- template -->
+
+<template>
+  <div id="layout">
+    <Header />
+  </div>
+</template>
+
 <style scoped>
-body {
-  background-color: white;
+#layout {
+  margin: ;
 }
 </style>
