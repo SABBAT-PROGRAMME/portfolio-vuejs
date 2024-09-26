08c2c616a7997dd1750d4b5426cfc6138289e0df
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-09-26 17:02:10 +0100

    add port

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: "0.0.0.0", // Écouter sur 0.0.0.0
+    port: process.env.PORT || 5173, // Utiliser le port attribué par Render ou 5173 par défaut
+  },
 });
